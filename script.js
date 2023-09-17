15a745e0ef6fd936b4967a4b705f18742248f695
yash yadav <132824732+yaduwanshiyash@users.noreply.github.com>
2023-09-17 14:46:34 +0530

    Update script.js
    new

--- script.js
+++ script.js
@@ -23,8 +23,6 @@ document.querySelector("#center").addEventListener("mousemove", throttleFunction
           "./istockphoto-465528647-1024x1024.jpg",
           "./pexels-chris-f-1283219.jpg",
       "https://plus.unsplash.com/premium_photo-1679355750936-77b24a624ed2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1940&q=80",
-      "./wallpaperflare.com_wallpaper (1).jpg",
-  "./wallpaperflare.com_wallpaper (2).jpg",
   "https://images.unsplash.com/photo-1694607140414-f8bfb76dfb22?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80",
   "https://plus.unsplash.com/premium_photo-1661344235865-7d203ccabebd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80",
   "https://images.unsplash.com/photo-1694537745659-65925587aa9e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1888&q=80",
